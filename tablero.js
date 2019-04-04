cc432b012b13c5eb0e78eb70e895fad2aeaf0e6f
abramaran <alberto.benavent@gmail.com>
2019-04-04 10:26:15 +0200

    Commit inicial
    Se dibuja el tablero, se colorea, se detecta la rotación de la pantalla y se activa la pantalla completa

---
+++ tablero.js
@@ -0,0 +1,56 @@
+/* function pantallaCompleta() {
+    var elem = document.documentElement;
+
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { // Firefox
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { // IE/Edge
+        elem.msRequestFullscreen();
+    }
+} */
+
+function pantallaCompleta() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+}
+
+$(function () {
+    let color = Math.floor(Math.random() * 359);
+    let colores = [];
+    let $casillas = $('.casilla');
+    
+    color = generarColores($casillas, colores, color);
+
+    $casillas.each(function (i) {
+        $(this).css('background-color', `hsl(${colores[i]}, 100%, 78%)`);
+
+        if ((i > 0 && i <= 5) || (i > 7 && i <= 12) || (i > 14)) {
+            $(this).css('border-left', '0px');
+        } else if (i == 6 || i == 13) {
+            $(this).css('border-top', '0px').css('border-bottom', '0px');
+        }
+    })
+})
+
+function generarColores($casillas, colores, color) {
+    for (let index = 0; index < $casillas.length; index++) {
+        colores.push(color);
+        color += 10;
+        if (color > 359)
+            color = 0;
+    }
+    let revertir = colores.slice(7, 13);
+    revertir.reverse();
+    revertir.forEach(function (elem, i) {
+        colores[i + 7] = elem;
+    });
+    return color;
+}
