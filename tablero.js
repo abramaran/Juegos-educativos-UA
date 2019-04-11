fa39a79b215626a5ac99ce851c61f2c99fa31cf9
abramaran <alberto.benavent@gmail.com>
2019-04-11 11:32:58 +0200

    Animación de los dados parametrizada
    Añadido segundo dado también y pulida la animación y el posicionamiento.

--- tablero.js
+++ tablero.js
@@ -30,8 +30,42 @@ $(function () {
     color = generarColores($casillas, colores, color);
 
     bordesCasillas($casillas, colores);
-    $('#cubilete').click(function() {
+    $('#cubilete').click(function(e) {
+        e.preventDefault;
+        let cube = document.querySelectorAll('.cube');
+        let wrap = document.querySelectorAll('.wrap');
+        let cubilete = $('#cubilete');
+        
+        for (let i = 0; i < wrap.length; i++) {
+            const icube = cube[i];
+            const iwrap = wrap[i];
+
+            icube.classList.remove('cube-anim');
+            iwrap.classList.remove('wrap-anim');
+            iwrap.style.top = cubilete.position().top - 10 + 'px';
+            iwrap.style.left = cubilete.position().left + (10 * i + 1) + 'px';
+            iwrap.style.zIndex = -1 - i;
+            iwrap.style.setProperty('--ydado', -40 + (Math.random() * 5 + 1) + 'vh');
+            
+            const randoms = (Math.random() * 4 + 4);
+            let equis = (Math.pow(-1, i + 1) * randoms + 'vh');
+            console.log(i + 1);
+            console.log(randoms);
+            console.log(equis);
+            
+            iwrap.style.setProperty('--xdado', equis);
+            iwrap.style.setProperty('--delayDado', (Math.random() * 0.7 + 0.4) + 's');
+        }        
+        void cube[0].offsetWidth;
+        void wrap[0].offsetWidth;
+        void cube[1].offsetWidth;
+        void wrap[1].offsetWidth;
+
         $('#cubilete').effect( "shake", {times:4, direction:'up'}, 1000 );
+        cube[0].classList.add('cube-anim');
+        wrap[0].classList.add('wrap-anim');
+        cube[1].classList.add('cube-anim');
+        wrap[1].classList.add('wrap-anim');
     })
 })
 
